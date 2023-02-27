f83574d72c2e66b2df7fef1b23235e09317f7967
shm_usuf <shmusuf@gmail.com>
2023-02-27 18:59:06 +0600

    auth hooks and context added

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { ProjectContextProvider } from "./context/ProjectContext";
 import "./index.css";
+import { ProjectContextProvider } from "./context/ProjectContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ProjectContextProvider>
-        <App />
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
       </ProjectContextProvider>
     </BrowserRouter>
   </React.StrictMode>
