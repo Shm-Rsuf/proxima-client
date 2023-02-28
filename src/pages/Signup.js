e8f9c48bd60afa7f6eb37f1f98a9142ddff49b2f
shm_usuf <shmusuf@gmail.com>
2023-02-28 11:11:10 +0600

    user signup aut  added

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,25 @@
 import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { signup, loading, error } = useSignup();
+
+  //handle signup
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    //signup user
+    await signup(email, password);
+  };
+
   return (
-    <form className="signup-form flex flex-col gap-5 py-10 max-w-sm mx-auto">
+    <form
+      onSubmit={handleSignup}
+      className="signup-form flex flex-col gap-5 py-10 max-w-sm mx-auto"
+    >
       <h2 className="text-sky-400 text-3xl capitalize font-medium mb-10">
         signup
       </h2>
@@ -18,6 +32,7 @@ const Signup = () => {
           Email address:
         </label>
         <input
+          required
           type="email"
           id="email"
           placeholder="abc@dev.com"
@@ -35,6 +50,7 @@ const Signup = () => {
           Password:
         </label>
         <input
+          required
           type="password"
           id="password"
           placeholder="Enter your password"
@@ -45,11 +61,17 @@ const Signup = () => {
       </div>
 
       <button
+        disabled={loading}
         type="submit"
         className="bg-sky-400 py-2 text-slate-900 rounded-md mt-3 hover:bg-sky-500 duration-300"
       >
         Signup
       </button>
+      {error && (
+        <p className="bg-rose-500/50 p-3 rounded-md border border-rose-500">
+          {error}
+        </p>
+      )}
     </form>
   );
 };
